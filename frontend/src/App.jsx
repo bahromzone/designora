14ff8256e4b78433b2b420ffadf6bdf8ed8dfdec
Bahromjon <dimaleks2525@gmail.com>
2026-07-10 20:53:00 +0500

    fix(search): remove obsolete auth page imports from routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import AppShell from "./components/AppShell";
 import ProtectedRoute from "./components/ProtectedRoute";
+import SearchShortcut from "./components/SearchShortcut";
 import BlogListPage from "./pages/BlogListPage";
 import BlogPostPage from "./pages/BlogPostPage";
 import CheckoutResultPage from "./pages/CheckoutResultPage";
@@ -13,33 +14,40 @@ import GlobalSearchPage from "./pages/GlobalSearchPage";
 import HomePage from "./pages/HomePage";
 import InstructorPage from "./pages/InstructorPage";
 import LearnPage from "./pages/LearnPage";
-import LoginPage from "./pages/LoginPage";
 import MyCoursesPage from "./pages/MyCoursesPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import PortfolioBuilderPage from "./pages/PortfolioBuilderPage";
 import ProfilePage from "./pages/ProfilePage";
 import PublicPortfolioPage from "./pages/PublicPortfolioPage";
-import RegisterPage from "./pages/RegisterPage";
 import VerifyPage from "./pages/VerifyPage";
 
 function App() {
-  return <AppShell><Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/qidiruv" element={<GlobalSearchPage />} />
-    <Route path="/kurslar" element={<CoursesPage />} />
-    <Route path="/kurslar/:courseId" element={<CourseDetailPage />} />
-    <Route path="/instruktor/:instructorId" element={<InstructorPage />} />
-    <Route path="/blog" element={<BlogListPage />} /><Route path="/blog/:slug" element={<BlogPostPage />} />
-    <Route path="/forum" element={<ForumListPage />} /><Route path="/forum/:threadId" element={<ForumThreadPage />} />
-    <Route path="/kirish" element={<LoginPage />} /><Route path="/royxatdan-otish" element={<RegisterPage />} /><Route path="/verify/:code" element={<VerifyPage />} />
-    <Route path="/portfolio/u/:userId" element={<PublicPortfolioPage />} />
-    <Route path="/kurslarim" element={<ProtectedRoute><MyCoursesPage /></ProtectedRoute>} />
-    <Route path="/organish/:courseId" element={<ProtectedRoute><LearnPage /></ProtectedRoute>} />
-    <Route path="/portfolio" element={<ProtectedRoute><PortfolioBuilderPage /></ProtectedRoute>} />
-    <Route path="/tolov/natija/:orderId" element={<ProtectedRoute><CheckoutResultPage /></ProtectedRoute>} />
-    <Route path="/profil" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
-    <Route path="*" element={<NotFoundPage />} />
-  </Routes></AppShell>;
+  return (
+    <AppShell>
+      <SearchShortcut />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/kurslar" element={<CoursesPage />} />
+        <Route path="/kurslar/:courseId" element={<CourseDetailPage />} />
+        <Route path="/instruktor/:instructorId" element={<InstructorPage />} />
+        <Route path="/blog" element={<BlogListPage />} />
+        <Route path="/blog/:slug" element={<BlogPostPage />} />
+        <Route path="/forum" element={<ForumListPage />} />
+        <Route path="/forum/:threadId" element={<ForumThreadPage />} />
+        <Route path="/qidiruv" element={<GlobalSearchPage />} />
+        <Route path="/kirish" element={<Navigate to="/" replace />} />
+        <Route path="/royxatdan-otish" element={<Navigate to="/" replace />} />
+        <Route path="/verify/:code" element={<VerifyPage />} />
+        <Route path="/portfolio/u/:userId" element={<PublicPortfolioPage />} />
+        <Route path="/kurslarim" element={<ProtectedRoute><MyCoursesPage /></ProtectedRoute>} />
+        <Route path="/organish/:courseId" element={<ProtectedRoute><LearnPage /></ProtectedRoute>} />
+        <Route path="/portfolio" element={<ProtectedRoute><PortfolioBuilderPage /></ProtectedRoute>} />
+        <Route path="/tolov/natija/:orderId" element={<ProtectedRoute><CheckoutResultPage /></ProtectedRoute>} />
+        <Route path="/profil" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </AppShell>
+  );
 }
 
 export default App;

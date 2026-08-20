8fada914e9ca0bc2ca1c4b902a2f2028d3646e43
Bahromjon <dimaleks2525@gmail.com>
2026-08-20 12:23:22 +0500

    style(auth): normalize URL formatting

--- frontend/src/lib/authExtra.js
+++ frontend/src/lib/authExtra.js
@@ -24,9 +24,7 @@ function loadRecaptchaScript() {
       return;
     }
     const script = document.createElement("script");
-    const recaptchaUrl = new URL(
-      "https://www.google.com/recaptcha/api.js",
-    );
+    const recaptchaUrl = new URL("https://www.google.com/recaptcha/api.js");
     recaptchaUrl.searchParams.set("render", siteKey);
     script.src = recaptchaUrl.toString();
     script.async = true;
